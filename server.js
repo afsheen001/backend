4e5e39ca0668e87e00bf206b39089b27173bdd91
afsheen001 <afsheenfatimaa10@gmail.com>
2025-12-08 19:34:15 +0400

    updated my code with CORS and JSON support after debugging few errors

--- server.js
+++ server.js
@@ -1,3 +1,18 @@
+
+// IMPORTS
+
+const express = require('express');
 const { MongoClient, ObjectId } = require('mongodb');
 const cors = require('cors');
 const path = require('path');
+
+// APP SETUP
+
+const app = express();
+const PORT = 3000;
+
+
+// MIDDLEWARE
+
+app.use(cors());
+app.use(express.json());
